988989255ca7ed91adad22a015acbe2d00653b73
mmujahiduddin <mohammed.mujahiduddin@hitachivantara.com>
2024-05-08 00:08:13 +0530

    [BACKLOG-39539][UPDATE][Java17Compatibility]

--- marketplace-core/src/test/java/org/pentaho/telemetry/TelemetryEventSenderTest.java
+++ marketplace-core/src/test/java/org/pentaho/telemetry/TelemetryEventSenderTest.java
@@ -35,8 +35,8 @@ import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
